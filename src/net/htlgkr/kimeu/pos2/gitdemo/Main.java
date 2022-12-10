f51f0d133b1c2840831038132c99c042eae8f8fa
Eugen Kimmerstorfer <e.kimmerstorfer@htl-grieskirchen.at>
2022-12-10 16:57:59 +0100

    second commit

--- src/net/htlgkr/kimeu/pos2/gitdemo/Main.java
+++ src/net/htlgkr/kimeu/pos2/gitdemo/Main.java
@@ -2,6 +2,6 @@ package net.htlgkr.kimeu.pos2.gitdemo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo, Git");
+        System.out.println("Hallo, Welt");
     }
 }
